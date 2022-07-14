9d66409c26eec1cf407672f6250d8ff0785e92d0
Timo1024 <47331071+Timo1024@users.noreply.github.com>
2022-07-14 18:27:08 +0200

    added first working command

---
+++ src/Commands.ts
@@ -0,0 +1,4 @@
+import { Command } from "./Command";
+import { Hello } from "./commands/hello";
+
+export const Commands: Command[] = [Hello];
---
+++ src/bot.ts
@@ -0,0 +1,15 @@
+import { Client, ClientOptions } from "discord.js";
+import ready from "./listeners/ready";
+import interactionCreate from "./listeners/interactionCreate";
+import { token } from "./auth.json"
+
+console.log("Bot is starting...");
+
+const client = new Client({
+    intents: []
+});
+
+ready(client);
+interactionCreate(client);
+
+client.login(token);
