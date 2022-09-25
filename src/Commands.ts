74ca4abd7094dfed3b0f64d6fa70e594151c304e
Robin Bonkaß <klapperschlange2820@gmail.com>
2022-09-25 14:01:31 +0200

    added first functional canvas command

--- src/Commands.ts
+++ src/Commands.ts
@@ -5,11 +5,11 @@ import { Command, UICommand } from "./Command";
 import { Hello } from "./commands/hello";
 import { UserInfo } from "./commands/user_data";
 import { Miesmuschel } from "./commands/miesmuschel";
-import { Canvas_test } from "./commands/canvas_test";
+import { CanvasTest } from "./commands/canvas_test";
 
 // TODO for all new MESSAGE UI commands
 import { MessageInfo } from "./commands/ui_message/message_info";
 
 // TODO for new command
 // add alls commands to this array
-export const Commands: (Command | UICommand)[] = [UserInfo, Hello, Miesmuschel, MessageInfo, Canvas_test];
+export const Commands: (Command | UICommand)[] = [UserInfo, Hello, Miesmuschel, MessageInfo, CanvasTest];
--- src/commands/canvas_test.ts
+++ src/commands/canvas_test.ts
@@ -1,19 +1,44 @@
 import { BaseCommandInteraction, Client } from "discord.js";
 import { Command } from "../Command";
-import { canvas_test } from "../parameters/commands.json"
+import { canvas_test } from "../parameters/commands.json";
+import { createCanvas, loadImage } from "canvas";
+import { MessageAttachment } from "discord.js";
 
 // TODO make dev command only
-export const Canvas_test: Command = {
+export const CanvasTest: Command = {
     name: canvas_test.name,
     description: canvas_test.description,
     type: "CHAT_INPUT",
     run: async (client: Client, interaction: BaseCommandInteraction) => {
-        
-        const content : string = "Moin, werde grade wiederbelebt... und bin jetzt über einen externen Server online";
 
-        await interaction.followUp({
-            ephemeral: true,
-            content
-        });
+        const can = createCanvas(1500, 1130);
+        const ctx = can.getContext("2d");
+
+        ctx.lineWidth = 8;
+        ctx.strokeStyle = "grey";
+
+        ctx.beginPath();
+        ctx.moveTo(300, 0);
+        ctx.lineTo(300, 900);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(600, 0);
+        ctx.lineTo(600, 900);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(0, 300);
+        ctx.lineTo(900, 300);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(0, 600);
+        ctx.lineTo(900, 600);
+        ctx.stroke();
+
+        const content = new MessageAttachment(can.toBuffer(), "image.png");
+
+        await interaction.editReply({ files: [content] });
     }
 }; 
