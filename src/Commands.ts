9bd821eecc4f707fdf949883e7f17e8bfec1e1cc
Robin Bonkaß <klapperschlange2820@gmail.com>
2022-09-27 13:04:48 +0200

    added command to view a specific archive

--- src/Commands.ts
+++ src/Commands.ts
@@ -7,10 +7,19 @@ import { UserInfo } from "./commands/user_data";
 import { Miesmuschel } from "./commands/miesmuschel";
 import { addArchive } from "./commands/archive/addArchive";
 import { editArchive } from "./commands/archive/editArchive";
+import { viewArchive } from "./commands/archive/viewArchive";
 
 // TODO for all new MESSAGE UI commands
 import { MessageInfo } from "./commands/ui_message/message_info";
 
 // TODO for new command
 // add alls commands to this array
-export const Commands: (Command | UICommand)[] = [UserInfo, Hello, Miesmuschel, MessageInfo, addArchive, editArchive];
+export const Commands: (Command | UICommand)[] = [
+    UserInfo, 
+    Hello, 
+    Miesmuschel, 
+    MessageInfo, 
+    addArchive, 
+    editArchive, 
+    viewArchive
+];
---
+++ src/commands/archive/viewArchive.ts
@@ -0,0 +1,69 @@
+import { BaseCommandInteraction, Client, MessageEmbed } from "discord.js";
+import { Command } from "../../Command";
+import { archive } from "../../parameters/commands.json";
+var fs = require("fs");
+
+export const viewArchive: Command = {
+    name: archive.viewArchive.name,
+    description: archive.viewArchive.description,
+    options: [
+        {
+            name: archive.viewArchive.options[0].name,
+            description: archive.viewArchive.options[0].description,
+            type: "INTEGER",
+            required: true
+        }
+    ],
+    type: "CHAT_INPUT",
+    run: async (client: Client, interaction: BaseCommandInteraction) => {
+
+        let readStream = fs.createReadStream("src/data/archive/main.archive", 'utf8');
+
+        let archiveChunk : string;
+
+        readStream.on('data', async function(chunk : string){
+            archiveChunk = chunk;
+        
+            // getting all options of the command
+            const number     : string = (interaction.options.get(archive.editArchive.options[0].name)?.value as number).toString();
+
+            // searching in the archive for the number given
+            let archiveSplitted : string[] = archiveChunk.split("\n");
+            let quote : string = "";
+            archiveSplitted.forEach((line : string) => {
+                if(line.startsWith(number + "|")){
+                    quote = line;
+                }
+            });
+
+            if(quote !== ""){
+                
+                const infos : string[] = quote.split("|");
+                const content : string = infos[1];
+                const person : string = infos[2];
+                const date : string = infos[3];
+    
+                const response : MessageEmbed = new MessageEmbed()
+                    .setColor("#64FF00")
+                    .addFields(
+                        { name: content, value : person + ", " + date }
+                    )
+                    .setFooter({
+                        text : '#' + number
+                    })
+    
+                await interaction.followUp({embeds: [ response ]});
+
+            } else {
+                let lastNumber : string = archiveSplitted.pop()?.split("|")[0] as string;
+                
+                await interaction.followUp({ 
+                    content: 'Didn\'t fing the quote. Use a number between 1 and ' + lastNumber, 
+                    ephemeral: true })
+            }
+
+
+        });
+        
+    }
+};
